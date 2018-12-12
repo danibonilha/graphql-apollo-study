66d97512922248837287ec14a064854bad1492eb
danibonilha <bonilha@work.co>
2018-12-12 14:26:25 -0200

    feat: Create mutation to add a new book

--- schema/index.js
+++ schema/index.js
@@ -1,11 +1,9 @@
-
-const { books } = require('./books');
+const { books } = require("./books");
 const { gql } = require("apollo-server");
 
 // Type definitions define the "shape" of your data and specify
 // which ways the data can be fetched from the GraphQL server.
 const typeDefs = gql`
-
   type Book {
     title: String
     author: String
@@ -14,17 +12,27 @@ const typeDefs = gql`
   type Query {
     books: [Book]
   }
+
+  type Mutation {
+    addBook(title: String, author: String): Book
+  }
 `;
 
 // Resolvers define the technique for fetching the types in the
 // schema.  We'll retrieve books from the "books" array above.
 const resolvers = {
   Query: {
-    books: () => books,
+    books: () => books
   },
+  Mutation: {
+    addBook(_, { title, author }) {
+      books.push({ title, author });
+      return { title, author };
+    }
+  }
 };
 
 module.exports = {
   typeDefs,
   resolvers
-}
+};
